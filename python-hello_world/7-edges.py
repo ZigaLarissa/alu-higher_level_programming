a1e0d03e956e0e7e1777f7fe81fe81f4ac8773ac
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-18 07:37:57 -0700

    copy cut paste

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "print(f"{word[0:3]}")"
-word_last_2 = "print(f"{word[-2:-1]}")"
-middle_word = "print(f"{word[1:-1]}")"
+word_first_3 = 'print(f"{word[0:3]}")'
+word_last_2 = 'print(f"{word[-2:-1]}")'
+middle_word = 'print(f"{word[1:-1]}")'
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
