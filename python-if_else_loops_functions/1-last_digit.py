f5137f82e88fb50a438d28b7a9ae7a1181a41340
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-20 02:02:11 -0700

    fixed a type error in this file

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number[-1]
+last_digit = str(number[-1])
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
