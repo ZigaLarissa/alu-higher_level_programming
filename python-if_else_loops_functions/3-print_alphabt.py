2c26fb343e4e68ac65bf3ce82c3ffba76e35cd8e
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-20 09:53:51 -0700

    omitting e and q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letters in range(97, 123):
-	if letters != 101 and letters != 113:
-		print("{}".format(chr(letters)), end='')
+ if letters != 101 and letters != 113:
+   print("{}".format(chr(letters)), end='')
