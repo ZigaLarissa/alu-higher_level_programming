46045db34e47e162545e2cce7dd6a6612c095981
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-20 08:22:44 -0700

    last digit game

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = str(number)
-digit = int(x[-1])
-if digit > 5:
-    print(f"Last digit of {number} is {x[-1]} and is greater than 5")
-elif digit == 0:
-    print(f"Last digit of {number} is {x[-1]} and is 0")
-elif digit < 6 and digit != 0:
-    print(f"Last digit of {number} is {x[-1]} and is less than 6 and not 0")
+digit = number[-1]
+if number < 0:
+    digit = -digit
+print(f"Last digit of {number} is {digit}", end='')
+    if digit > 5:
+        print("and is greater than 5")
+    elif digit == 0:
+        print("and is 0")
+    else:
+        print("and is less than 6 and not 0")
