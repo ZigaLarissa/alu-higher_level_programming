d2b912904d0d26f8d2673448ae7d05be12b6701f
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-20 02:08:59 -0700

    fixed a type error in this file

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = str(number[-1])
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+x = str(number)
+if x[-1] > 5:
+    print(f"Last digit of {x} is {x[-1]} and is greater than 5")
+elif x[-1] == 0:
+    print(f"Last digit of {x} is {x[-1]} and is 0")
+elif x[-1] < 6 and x[-1] != 0:
+    print(f"Last digit of {x} is {x[-1]} and is less than 6 and not 0")
