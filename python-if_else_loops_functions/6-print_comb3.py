9b3ee11a997daa7ac3e9d5bccbc84c0ebb3b135a
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-21 01:19:03 -0700

    manipulating digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(0, 10):
     for y in range(x+1, 10):
         if x == 8 and y == 9:
-            print("{}{}".format(x,y))
+            print("{}{}".format(x, y))
         else:
-            print("{}{}".format(x,y), end=', ')
+            print("{}{}".format(x, y), end=', ')
