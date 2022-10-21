0ebae48cb22cdead0d07f9714eac212ac0834557
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-21 05:55:49 -0700

    FizzBuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for n in range(1,101):
-        if n % 3 == 0 and n % 5 ==0:
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=' ')
         elif n % 5 == 0:
             print("Buzz", end=' ')
