745d9144bdfd6b44e8b96a5de7d836c79083c213
Ziga Larissa <l.bizimungu@alustudent.com>
2022-10-20 23:37:50 -0700

    two digits

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
-    print("{:02d}".format(n), end=', ')
+    if n == 99:
+        print("{}".format(n))
+    else:
+        print("{:02d}".format(n), end=', ')
