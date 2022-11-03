85522b0b098502fe901a1ce420e0fdd42a029a71
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-03 00:43:25 -0700

    more returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
     if length == 0:
         first = None
+    else:
+        first = sentence[0]
+
     return(length, first)
