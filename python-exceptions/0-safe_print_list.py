3036033f311e53392c77497f729415f4b69f607c
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-07 06:43:44 -0800

    safe printing an integer list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             func = func + 1
-        except IndexError:
+        except:
             break
     print("")
     return (func)
