6e7d8451b5ab2f521118f84910320ac5d9e6297a
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-08 07:29:13 -0800

    printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by"""
+
+
+class Square:
+    """Write a class Square that defines a square by"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for s in range(self.__size):
+                    print('#', end="")
+                print()
