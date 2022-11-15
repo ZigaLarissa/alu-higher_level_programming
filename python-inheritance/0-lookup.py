d95c79d6175a1dbf865c05cf35cf023665d38e00
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-15 07:24:15 -0800

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a function that returns the list of available attributes and methods of an object"""
+"""returns the list of available attributes and methods of an object"""
 
 
 def lookup(obj):
