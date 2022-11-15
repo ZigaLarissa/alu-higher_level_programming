67de002dc861fb5c31626a50b5ac0d2ac869e66a
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-15 09:55:50 -0800

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""about to make a non empty class"""
+
+
+class BaseGeometry:
+    """define the BaseGeometry class"""
+
+    def area(self):
+        """Not implemeted"""
+
+        raise Exception("area() is not implemented")
