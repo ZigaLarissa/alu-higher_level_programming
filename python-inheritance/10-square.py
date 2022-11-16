e2bdf17a6b2df4b4deaeeeee94d78c8008d8594b
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-16 01:43:03 -0800

    Square #1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Inherit from the Rectangle class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """create a subclass Square from Rectangle"""
+
+    def __init__(self, size):
+        """make the size implementation"""
+        
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
