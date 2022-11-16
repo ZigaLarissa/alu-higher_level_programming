244a87b0388bfc8d36802b5647e765d95138b7cd
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-16 08:03:27 -0800

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Create a file that reads contents of another."""
+
+
 def read_file(filename=""):
+    """Open and read the said file"""
+
     with open(filename, encoding='UTF8') as f:
         print(f.read(), end="")
