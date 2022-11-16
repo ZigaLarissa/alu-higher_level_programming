c2906279ccd8ad0c31e75863d6bb59d295170ab7
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-16 09:31:07 -0800

    Save Object to file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write an Object to text file using JSON"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write object in text file using JSON representation"""
+    
+    with open(filename, 'w+') as handle:
+        return handle.write(json.dumps(my_obj))
