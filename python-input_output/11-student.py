d28c8dd02e798532b8d73e65bb80077919041572
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-17 01:01:24 -0800

    student to disk and load

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class that returns the dictionary of attributes of instances"""
+
+
+class Student:
+    """Define class attributes."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Method that retries the attributes dic"""
+    def to_json(self, attrs=None):
+        dic = self.__dict__
+
+        if attrs is None and type(attrs) != list:
+            return(dic)
+
+        else:
+            new_dic = dict()
+            for item in attrs:
+                if item in dic.keys():
+                    new_dic[item] = dic[item]
+            return(new_dic)
+
+    """Method that replaces all attributes of an instance."""
+    def reload_from_json(self, json):
+        return(json)
