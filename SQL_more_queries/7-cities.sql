f816d70f5867b0efc54c3959620d3e1f9f77daac
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-25 02:47:56 -0800

    cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
---- list the cities 
+-- list the cities 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
