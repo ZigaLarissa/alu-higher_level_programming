f8aa320342c679da966f70162f9fc755ea98bfd2
Ziga Larissa <l.bizimungu@alustudent.com>
2022-11-25 11:38:37 -0800

    No Comedy tonight!

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,18 @@
+-- script that lists all shows without the genre 'Comedy' in the database 'hbtn_0d_tvshows'
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
