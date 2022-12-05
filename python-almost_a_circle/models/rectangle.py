ad5ed64eadde7d360876ab81c77c9ed4072a7301
Ziga Larissa <l.bizimungu@alustudent.com>
2022-12-05 05:21:33 -0800

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""import base from models."""
+
+from models.base import base
+
+
+class Rectangle(Base):
+    """A new class that inherites from base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the attributes."""
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Access the width."""
+
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the height attr."""
+
+        return self.__height
+
+    @property
+    def x(self):
+        """Get the x attr."""
+
+        return self.__x
+
+    @property
+    def y(self):
+        """Get the y attr."""
+
+        return self.__y
