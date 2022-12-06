7e4e14666c89fb08ccdc3d77c29c899c05743151
Ziga Larissa <l.bizimungu@alustudent.com>
2022-12-06 07:41:17 -0800

    Filter states.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all states that starts with N."""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    my_connect = MySQLdb.connect(
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            host="localhost",
+            port=3306
+        )
+
+    my_cursor = my_connect.cursor()
+    my_cursor.execute("SELECT * FROM states \
+            WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = my_cursor.fetchall()
+
+    for i in result:
+        print(i)
+    my_connect.close()
