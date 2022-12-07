7e94d2c9dccf0a5bc23f1d4b62b81aea0b60550d
Ziga Larissa <l.bizimungu@alustudent.com>
2022-12-07 02:00:07 -0800

    Add a new state

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that adds the State object
+“Louisiana” to the database hbtn_0e_6_usa."""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    my_engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    
+    Session = sessionmaker(bind=my_engine)
+    my_session = Session()
+    Base.metadata.create_all(my_engine)
+
+    state = State(name="Louisiana")
+    my_session.add(state)
+    my_session.commit()
+    print(state.id)
+    my_session.close()
