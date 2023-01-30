b315e4fdb9fd8e00be27ab849a51124697fb05d4
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-30 05:49:21 -0800

    Arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-let numArgv = process.argv.length;
-if (numArgv < 3)
-	console.log('No argument');
-else if (numArgv === 3)
-	console.log('Argument found');
-else
-	console.log('Arguments found');
+const numArgv = process.argv.length;
+if (numArgv < 3) { console.log('No argument'); } else if (numArgv === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
