dd5327ba500a974e0dd0824f90f64131eaeb9b93
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-30 11:12:32 -0800

    An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
-if (Number.isNaN(num))
-  console.log('Not a number');
-else
-  console.log('My number: ' + num);
+if (Number.isNaN(num)) { console.log('Not a number'); } else { console.log('My number: ' + num); }
