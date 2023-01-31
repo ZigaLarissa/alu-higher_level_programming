8fed30bacfba10f8e7b94296db0738307986d51b
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-31 08:03:19 -0800

    Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const myArray = process.argv;
 if (myArray.length <= 3) {
-  console.log(0)
-}
-else {
+  console.log(0);
+} else {
   console.log(myArray.sort((a, b) => b - a).slice(3)[0]);
 }
