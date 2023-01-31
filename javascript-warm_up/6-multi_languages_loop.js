39b7096acf22f8c1548b5f752e5e215430b595e0
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-30 23:48:11 -0800

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-for (let i = 0; i < 3; i++)
-  if (i == 0) console.log("C is fun");
-  else if (i == 1) console.log("Python is cool");
-  else console.log("JavaScript is amazing");
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++)
+  console.log(myArray[i]);
