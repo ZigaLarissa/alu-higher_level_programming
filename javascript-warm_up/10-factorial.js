bfa7860dd605fc9945e6adf86f632bd9d3c79483
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-31 07:53:23 -0800

    Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
-function factorialize(num) {
+function factorialize (num) {
   let factorial = 1;
   for (let i = 1; i <= num; i++) {
     factorial *= i;
