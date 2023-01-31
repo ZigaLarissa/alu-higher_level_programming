7844a315ad1039805b8dfe922c0c63011e5ce387
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-31 00:27:36 -0800

    I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const firstArgv = process.argv[2]
-if (isNaN(firstArgv))
-  console.log('Missing number of occurrences');
-else
-  for (let i = 0; i < firstArgv; i++)
-    console.log('C is fun');
+const firstArgv = process.argv[2];
+if (isNaN(firstArgv)) { console.log('Missing number of occurrences'); } else {
+  for (let i = 0; i < firstArgv; i++) { console.log('C is fun'); }
+}
