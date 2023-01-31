2656e0f5ec50c872b7bb2eaf44fbce6824e83544
Ziga Larissa <ziga0902larissa@gmail.com>
2023-01-31 00:44:14 -0800

    Square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints a square
+const size = process.argv[2];
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
