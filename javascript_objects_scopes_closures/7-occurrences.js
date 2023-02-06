52e41208301b63db6b5d763fde9221b8aa8ed080
Ziga Larissa <ziga0902larissa@gmail.com>
2023-02-06 06:38:16 -0800

    Occurrences

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//returns the number of occurrences in a list
+// returns the number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
   let occurrence = 0;
   for (let i = 0; i < list.length; i++) {
