054a104eebd5def3c26556f15cbbed1160508503
Ziga Larissa <ziga0902larissa@gmail.com>
2023-02-06 07:28:09 -0800

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-/* script that prints 3 lines: (like 1-multi_languages.js)
-but by using an array of string and a loop */
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
