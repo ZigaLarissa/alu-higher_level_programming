1dd67d061c62ae96d7efbe72a15c809955232034
Ziga Larissa <ziga0902larissa@gmail.com>
2023-02-06 07:21:44 -0800

    Log me

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* function that converts a number from base 10
+to another base passed as argument */
+exports.converter = function (base) {
+  return function (argv) {
+    return argv.toString(base);
+  };
+};
