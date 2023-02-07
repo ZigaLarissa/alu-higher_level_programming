a2f486fd5bd2a71d24de0949c4a364be741a0859
Ziga Larissa <ziga0902larissa@gmail.com>
2023-02-07 09:58:22 +0000

    Readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let myFile = process.argv[2];
+const fs = require('fs');
+fs.readFile(myFile, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data)
+});
+
