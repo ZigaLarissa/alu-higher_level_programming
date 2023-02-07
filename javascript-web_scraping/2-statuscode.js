fc2a707e310f7c01e54a011c16bd686757a2ef5b
Ziga Larissa <ziga0902larissa@gmail.com>
2023-02-07 11:42:04 +0000

    Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const req = require('request');
-req.get(url,  (err, res) => {
+req.get(url, (err, res) => {
   if (err) {
-    console.log('code: '+ err);
+    console.log('code: ' + err);
   }
-  
-  console.log('code: ' + res.statusCode)
+
+  console.log('code: ' + res.statusCode);
 });
