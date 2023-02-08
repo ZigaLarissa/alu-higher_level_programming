ceb6462aa39a2332ac04302b5e26c3870d9abf40
Ziga Larissa <ziga0902larissa@gmail.com>
2023-02-08 08:55:15 +0000

    Loripsum

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const url = process.argv[2];
-const fileName = process.argv[3]
+const fileName = process.argv[3];
 const req = require('request');
 const fs = require('fs');
 req.get(url, (err, res, body) => {
